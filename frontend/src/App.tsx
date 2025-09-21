007d07633914a413815b580d90272652b531b78a
Gayoung Lee <gayounglee57@gmail.com>
2025-09-21 21:43:32 +1200

    Added App.tsx to FE project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="max-w-3xl mx-auto my-6 font-sans">
+      <h1 className="text-2xl font-bold mb-4">Order Processing App</h1>
+      {/* <OrderForm /> */}
+      <hr className="my-4 border-gray-200" />
+      {/* <OrdersList orders={orders} /> */}
+    </div>
+  );
+}
